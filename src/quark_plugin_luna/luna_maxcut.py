154325f391ce51620c87cee7f19e471a6faa34c7
Daniel Porawski <d.porawski@gmx.de>
2025-08-28 16:35:03 +0200

    maxcut fix: WIP

--- src/quark_plugin_luna/luna_maxcut.py
+++ src/quark_plugin_luna/luna_maxcut.py
@@ -9,6 +9,7 @@ from luna_quantum import Model, LunaSolve
 from luna_quantum.translator import LpTranslator
 
 import random
+import networkx as nx
 
 from .utils import get_luna_api_key
 
@@ -33,37 +34,44 @@ class LunaMaxCut(Core):
     def generate_graph(self, num_nodes, edge_prob, weight_range, seed):
         if seed is not None:
             random.seed(seed)
-        edges = []
+        G = nx.Graph()
+        G.add_nodes_from(range(num_nodes))
         for i in range(num_nodes):
             for j in range(i + 1, num_nodes):
                 if random.random() < edge_prob:
-                    weight = random.randint(*weight_range)
-                    edges.append((i, j, weight))
-        if not edges:
-            # ensure at least one edge exists
-            edges.append((0, 1, random.randint(*weight_range)))
-        self.edges = edges
+                    w = random.randint(*weight_range)
+                    G.add_edge(i, j, weight=w)
+        if G.number_of_edges() == 0:
+            G.add_edge(0, 1, weight=str(random.randint(*weight_range)))
+        self.graph = G
 
     @override
     def preprocess(self, data: InterfaceType = None) -> Result:
         self.generate_graph(self.num_nodes, self.edge_prob, self.weight_range, self.seed)
         LunaSolve.authenticate(get_luna_api_key())
         ls = LunaSolve()
-
-        maxcut = MaxCut(num_nodes=self.num_nodes, edges=self.edges)
+        str_graph = nx.relabel_nodes(self.graph, lambda x: str(x))
+        
+        graph_dict = nx.to_dict_of_dicts(str_graph)
+        print("STR_GRAPH: ", graph_dict)
+        maxcut = MaxCut(graph=graph_dict)
         meta_model = ls.model.create_from_use_case(name="MaxCut", use_case=maxcut)
         model = Model.load_luna(model_id=meta_model.id)
         lp_model = LpTranslator.from_aq(model)
+        print("LP_MODEL: ", lp_model)
         return Data(Other[str](lp_model))
 
     @override
     def postprocess(self, data: InterfaceType) -> Result:
         lp_solution = data.data
+        print("LP_SOLUTION: ", lp_solution)
         if lp_solution is None:
             return Failed("No solution found")
 
         partition = {i: 1 if lp_solution.get(f"x_{i}", 0.0) >= 0.5 else 0 for i in self.graph.nodes()}
-        cut_edges = [(u, v) for (u, v) in self.graph.edges() if partition[u] != partition[v]]
+        cut_value = 0
+        for u, v, w in self.graph.edges(data="weight", default=1):
+            if partition[u] != partition[v]:
+                cut_value += w
 
-        obj_value = len(cut_edges)  # unweighted case
-        return Data(Other(obj_value))
+        return Data(Other(cut_value))
