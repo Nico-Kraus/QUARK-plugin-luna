df2396d2d38f66d84e2061b29e65e41f0b74db25
Daniel Porawski <d.porawski@gmx.de>
2025-08-28 12:31:04 +0200

    setpacking evaluation fix

--- src/quark_plugin_luna/luna_setpacking.py
+++ src/quark_plugin_luna/luna_setpacking.py
@@ -63,12 +63,15 @@ class LunaSetPacking(Core):
         meta_model = ls.model.create_from_use_case(name="Set Packing", use_case=set_packing)
         model = Model.load_luna(model_id=meta_model.id)
         lp_model = LpTranslator.from_aq(model)
+        print(self.subset_matrix)
+        print(lp_model)
         return Data(Other[str](lp_model))
 
     @override
     def postprocess(self, data: InterfaceType) -> Result:
             
         lp_solution = data.data
+        print(lp_solution)
         if lp_solution is None:
             return Failed("No solution found")
         
@@ -77,11 +80,14 @@ class LunaSetPacking(Core):
             val = lp_solution.get(f"x_{i}", 0.0)
             solution.append(1 if val >= 0.5 else 0) 
 
-        for row in self.subset_matrix:
-            total = sum(x * val for x, val in zip(solution, row))
-            if total > 1:
-                return Failed("Invalid solution.")
-            
+        result = [0] * len(self.subset_matrix[0])
+
+        for i, flag in enumerate(solution):
+            if flag == 1:
+                for j in range(len(self.subset_matrix[0])):
+                    result[j] += self.subset_matrix[i][j]
+                    if result[j] > 1:
+                        return Failed("Invalid solution: overlapping subsets.")
         obj_value = sum(w * x for w, x in zip(self.subset_weights, solution))
         return Data(Other(obj_value))
 
